13c0855a8c7b80b1aeb6fe4fd9a4bb62f30e8402
jun447 <junaidmumtaz16@gmail.com>
2023-10-14 15:00:40 +0500

    State reset functionality done

--- App.js
+++ App.js
@@ -12,10 +12,10 @@ const validationSchema = Yup.object().shape({
 
 export default function App() {
 
-  const [password, setPassword] = useState(0);
+  const [password, setPassword] = useState('');
 
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
-  const [lowerCase, setLowerCase] = useState(false);
+  const [lowerCase, setLowerCase] = useState(true);
   const [upperCase, setUpperCase] = useState(false);
   const [number, setNumber] = useState(false);
   const [specialChar, setSpecialChar] = useState(false);
@@ -53,10 +53,14 @@ export default function App() {
   }
   
   const resetPasswordState = () => {  
-    setLowerCase(false);
+  //  write all state reset code here
+   setIsPasswordVisible(false);
+   setPassword('');
+    setLowerCase(true);
     setUpperCase(false);
     setNumber(false);
     setSpecialChar(false);
+
 
   }
 
@@ -69,10 +73,96 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
+  },
+  formContainer: {
+    margin: 8,
+    padding: 8,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: '600',
+    marginBottom: 15,
+  },
+  subTitle: {
+    fontSize: 26,
+    fontWeight: '600',
+    marginBottom: 2,
+  },
+  description: {
+    color: '#758283',
+    marginBottom: 8,
+  },
+  heading: {
+    fontSize: 15,
+  },
+  inputWrapper: {
+    marginBottom: 15,
     alignItems: 'center',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+  },
+  inputColumn: {
+    flexDirection: 'column',
+  },
+  inputStyle: {
+    padding: 8,
+    width: '30%',
+    borderWidth: 1,
+    borderRadius: 4,
+    borderColor: '#16213e',
+  },
+  errorText: {
+    fontSize: 12,
+    color: '#ff0d10',
+  },
+  formActions: {
+    flexDirection: 'row',
     justifyContent: 'center',
+  },
+  primaryBtn: {
+    width: 120,
+    padding: 10,
+    borderRadius: 8,
+    marginHorizontal: 8,
+    backgroundColor: '#5DA3FA',
+  },
+  primaryBtnTxt: {
+    color: '#fff',
+    textAlign: 'center',
+    fontWeight: '700',
+  },
+  secondaryBtn: {
+    width: 120,
+    padding: 10,
+    borderRadius: 8,
+    marginHorizontal: 8,
+    backgroundColor: '#CAD5E2',
+  },
+  secondaryBtnTxt: {
+    textAlign: 'center',
+  },
+  card: {
+    padding: 12,
+    borderRadius: 6,
+    marginHorizontal: 12,
+  },
+  cardElevated: {
+    backgroundColor: '#ffffff',
+    elevation: 1,
+    shadowOffset: {
+      width: 1,
+      height: 1,
+    },
+    shadowColor: '#333',
+    shadowOpacity: 0.2,
+    shadowRadius: 2,
+  },
+  generatedPassword: {
+    fontSize: 22,
+    textAlign: 'center',
+    marginBottom: 12,
+    color:'#000'
   },
 });
